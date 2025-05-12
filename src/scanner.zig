8e2c84bead663ecdb5764bb068866c282c9ad3f8
KaFai Choi <kafaicoder@gmail.com>
2025-05-12 11:56:57 +0700

    implement scanToken in scanning chapter

--- src/scanner.zig
+++ src/scanner.zig
@@ -1,25 +1,78 @@
 const std = @import("std");
 
-pub const TokenType = enum { LEFT_PAREN, RIGHT_PARENS, EOF, ERROR };
+pub const TokenType = enum {
+    // Single-character tokens
+    LEFT_PAREN,
+    RIGHT_PARENS,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // One or two character tokens
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    EOF,
+    ERROR,
+};
 
 pub const Token = struct {
     type: TokenType,
     lexeme: []const u8,
     line: usize,
 
-    pub fn init(token_type: TokenType) Token {
-        return Token{ .type = token_type, .lexeme = "", .line = 0 };
+    pub fn init(token_type: TokenType, lexeme: []const u8, line: usize) Token {
+        return Token{ .type = token_type, .lexeme = lexeme, .line = line };
     }
 };
 
 pub const Scanner = struct {
     source: []const u8,
+    start: usize,
     curr: usize,
+    line: usize,
 
     pub fn init(source: []const u8) !Scanner {
         return Scanner{
             .source = source,
+            .start = 0,
             .curr = 0,
+            .line = 1,
         };
     }
 
@@ -27,26 +80,54 @@ pub const Scanner = struct {
         var tokens = std.ArrayList(Token).init(std.heap.page_allocator);
 
         while (!self.isAtEnd()) {
+            // we start at the beginning of the current token
+            self.start = self.curr;
             const token = self.scanToken();
             try tokens.append(token);
         }
 
-        try tokens.append(Token.init(TokenType.EOF));
+        try tokens.append(Token.init(TokenType.EOF, "", self.line));
         return tokens;
     }
 
     pub fn scanToken(self: *Scanner) Token {
-        defer self.curr += 1;
+        const c = self.advance();
 
-        if (self.source[self.curr] == '(') {
-            return Token.init(TokenType.LEFT_PAREN);
-        }
+        const token = switch (c) {
+            '(' => self.addToken(TokenType.LEFT_PAREN),
+            ')' => self.addToken(TokenType.RIGHT_PARENS),
+            '{' => self.addToken(TokenType.LEFT_BRACE),
+            '}' => self.addToken(TokenType.RIGHT_BRACE),
+            ',' => self.addToken(TokenType.COMMA),
+            '.' => self.addToken(TokenType.DOT),
+            '-' => self.addToken(TokenType.MINUS),
+            '+' => self.addToken(TokenType.PLUS),
+            ';' => self.addToken(TokenType.SEMICOLON),
+            '*' => self.addToken(TokenType.STAR),
+            '!' => self.addToken(TokenType.BANG),
+            '=' => self.addToken(TokenType.EQUAL),
+            else => self.addToken(TokenType.ERROR),
+        };
 
-        if (self.source[self.curr] == ')') {
-            return Token.init(TokenType.RIGHT_PARENS);
-        }
+        return token;
+    }
 
-        return Token.init(TokenType.ERROR);
+    fn addToken(self: *Scanner, token_type: TokenType) Token {
+        const lexeme = self.source[self.start..self.curr];
+        return Token.init(token_type, lexeme, self.line);
+    }
+
+    fn advance(self: *Scanner) u8 {
+        const c = self.source[self.curr];
+        self.curr += 1;
+        return c;
+    }
+
+    fn match(self: *Scanner, expected: u8) bool {
+        if (self.isAtEnd()) return false;
+        if (self.source[self.curr] != expected) return false;
+        self.curr += 1;
+        return true;
     }
 
     fn isAtEnd(self: *Scanner) bool {
